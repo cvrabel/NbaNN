b69306aafcd2b9ddcd2e5f158a78fde2373008b1
chris <cjv38@cornell.edu>
2016-03-08 12:41:48 -0500

    NBA Neural Network

---
+++ NeuralNetwork.py
@@ -0,0 +1,176 @@
+import numpy as np
+
+class Neural_Network(object):
+    def __init__(self, Lambda, inputSize, hiddenSize, outputSize):
+
+        self.inputLayerSize = inputSize
+        self.outputLayerSize = outputSize
+        self.hiddenLayerSize = hiddenSize
+  
+        #Weights (parameters)
+        #W1 for weights from input to hidden (initialized as random)
+        self.W1 = np.random.randn(self.inputLayerSize,self.hiddenLayerSize)
+        #W2 for weights from hidden to output (initialized as random)
+        self.W2 = np.random.randn(self.hiddenLayerSize,self.outputLayerSize)
+        
+        #Regularization Parameter:
+        self.Lambda = Lambda
+        
+    def propogation(self, inputX):
+        #Propogate inputs though network
+
+        self.secondLayer = np.dot(inputX, self.W1)
+        #Apply activation function
+        self.sigmoidSecond = 1/(1+np.exp(self.secondLayer))
+
+        self.thirdLayer = np.dot(self.sigmoidSecond, self.W2)
+        #Apply activation function
+        yHat = 1/(1+np.exp(self.thirdLayer))
+        return yHat
+        
+    
+    def sigmoidPrime(self,z):
+        #Gradient of sigmoid (get the derivative)
+        return np.exp(-z)/( (1+np.exp(-z))**2 )
+    
+    def costFunction(self, X, y):
+        #Compute how much error for given X,y, use weights already stored in class.
+        #Need to minimize error(cost)
+        self.yHat = self.propogation(X)
+
+        '''Square the errors so cannot get stuck in local min when performing 
+        gradient descent.  Will make the graph convex.'''
+        error = 0.5*np.sum( (y-self.yHat)**2 )/X.shape[0] + \
+                (self.Lambda/2)*( np.sum(self.W2**2)+np.sum(self.W2**2) )
+        return error
+        
+    def costFunctionPrime(self, X, y):
+        # print('cost')
+        '''Compute partial derivative with respect to W and W2 for a given X and y:
+        Gradient descent method: check error at steps and stop when error stops getting smaller.'''
+        
+        self.yHat = self.propogation(X)
+        errorThirdLayer = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.thirdLayer))
+        partialDerivW2 = np.dot(self.sigmoidSecond.T, errorThirdLayer)/X.shape[0] + self.Lambda*self.W2
+
+        errorSecondLayer = np.dot(errorThirdLayer, self.W2.T)*self.sigmoidPrime(self.secondLayer)
+        partialDerivW1 = np.dot(X.T, errorSecondLayer)/X.shape[0] + self.Lambda*self.W1
+        
+        return partialDerivW1, partialDerivW2
+    
+    #Helper functions for interacting with other methods/classes
+    def getParams(self):
+        #Get W1 and W2 Rolled into vector using ravel
+        params = np.concatenate((self.W1.ravel(), self.W2.ravel()))
+        return params
+    
+    def setParams(self, params):
+        #Set W1 and W2 using single parameter vector:
+
+        W1_final = self.hiddenLayerSize*self.inputLayerSize #Number of weights in W1
+        self.W1 = np.reshape(params[0:W1_final], (self.inputLayerSize, self.hiddenLayerSize))
+
+        W2_final = W1_final + self.hiddenLayerSize*self.outputLayerSize #Number of weights in W2
+        self.W2 = np.reshape(params[W1_final:W2_final], (self.hiddenLayerSize, self.outputLayerSize))
+        
+    def computeGradients(self, X, y):
+        #Get the two gradients from the cost function 
+        #then return them vectorized
+        partialDerivW1, partialDerivW2 = self.costFunctionPrime(X, y)
+        return np.concatenate((partialDerivW1.ravel(), partialDerivW2.ravel()))
+
+    def computeNumericalGradient(NN, X, y):
+        W1W2 = NN.getParams()
+
+        numgrad = np.zeros(W1W2.shape)
+        movement = np.zeros(W1W2.shape)
+        e = 1e-4
+        #Test 1 gradient at a time for gradient descent
+        for i in range(len(W1W2)):
+            #Set perturbation vector
+            movement[i] = e
+
+            #Add and subtract epsilon then compute cost
+            N.setParams(W1W2 + movement)
+            loss2 = NN.costFunction(X, y)
+
+            N.setParams(W1W2 - movement)
+            loss1 = NN.costFunction(X, y)
+
+            #Find slope between two costs
+            numgrad[i] = (loss2 - loss1) / (2*e)
+
+            #Return the value we changed to zero:
+            movement[i] = 0
+            
+        #Return Params to original value:
+        NN.setParams(W1W2)
+
+        #Return matrix of size of Weights
+        return numgrad 
+        
+## ----------------------- Part 6 ---------------------------- ##
+from scipy import optimize
+
+
+##Need to modify trainer class a bit to check testing error during training:
+class trainer(object):
+    def __init__(self, NeuralNetwork):
+        #Make Local reference to network:
+        self.NN = NeuralNetwork
+        
+    def callbackF(self, params):
+        self.NN.setParams(params)
+        self.Costs.append(self.NN.costFunction(self.X, self.y))
+        self.testCosts.append(self.NN.costFunction(self.testX, self.testY))
+        
+    def costFunctionWrapper(self, params, X, y):
+        #Needed for the scipy optimize method
+        self.NN.setParams(params)
+        error = self.NN.costFunction(X, y)
+        gradient = self.NN.computeGradients(X,y)
+        
+        return error, gradient
+        
+    def train(self, trainX, trainY, testX, testY):
+        #Make an internal variable for the callback function:
+        self.X = trainX
+        self.y = trainY
+        
+        self.testX = testX
+        self.testY = testY
+
+        #Make empty list to store training costs:
+        self.Costs = []
+        self.testCosts = []
+        
+        #Use the scipy optimize function
+        #Use BFGS to estimate the curvature and useful for batch descent
+        params0 = self.NN.getParams()
+        options = {'maxiter': 600, 'disp' : False}
+
+        #First parameter must be a function that accepts vector of parameters and in/out data
+        #and returns the costs and gradients 
+        _res = optimize.minimize(self.costFunctionWrapper, params0, jac=True, method='BFGS', \
+                                 args=(trainX, trainY), options=options, callback=self.callbackF)
+
+        self.NN.setParams(_res.x)
+        self.optimizationResults = _res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ autoPredict.py
@@ -0,0 +1,235 @@
+from NeuralNetwork import Neural_Network,trainer
+import numpy as np
+import csv
+from numpy.lib.npyio import genfromtxt
+import os
+import pandas as pd
+#season_01 = genfromtxt('season 01.csv', skip_header=1, dtype = float, delimiter=',')
+#season_2 = genfromtxt('season 2.csv', skip_header=1, dtype = float, delimiter=',')
+
+def vectorOfStats(tempRow, predictors):
+	vector = []
+	for p in predictors:
+		vector.append(int(tempRow[p]))
+	return vector
+
+def predictStat(playerStats, allStats, stat1, predictors):
+	testVector = []
+	copyTest = []
+	seasonNum = len(playerStats.index)-1
+	myPos = playerStats.iloc[[seasonNum]]['Pos'].to_string()[-1]
+	if(myPos == 'F'):
+		pos=0 
+	elif(myPos == 'C'):
+		pos=1
+	else:
+		pos=2
+
+	#---------Handling the player's stats-----------#
+	for i in range(seasonNum):
+		tempRow0 = playerStats.iloc[[i]]
+		testVector.append(vectorOfStats(tempRow0, predictors))
+		copyTest.append([int(tempRow0[stat1])])
+
+	testVector = np.array(testVector)
+	testVector = testVector.flatten()
+	numInputs = (seasonNum)*len(predictors)
+
+	if(seasonNum > 16):
+		copyTest = copyTest[len(copyTest)-2:]
+	elif(seasonNum > 8):
+		copyTest = copyTest[len(copyTest)-3:]
+	elif(seasonNum > 5):
+		copyTest = copyTest[len(copyTest)-5:]
+
+	if(seasonNum > 16):
+		numInputs = len(predictors)*3
+		testVector = testVector[(len(testVector)-numInputs):]
+	elif(seasonNum > 9):
+		numInputs = len(predictors)*6
+		testVector = testVector[(len(testVector)-numInputs):]
+
+	#---------End Handling the player's stats-----------#	
+
+
+	trainingX = []
+	trainingY = []
+
+
+	#---------Getting stats from all players-----------#
+	numPlayers = len(allStats.index)	
+	prevId = "null"
+	tempX = []
+	samePlayer = True
+	prevSeason = 0
+
+	# Loop through all players
+	for i in range(numPlayers):
+	    tempRow0 = allStats.iloc[[i]]
+	    myId = tempRow0["ilkid"].to_string()[8:]
+	    mySeason = int(tempRow0["seasonNum"])
+
+	    # Use position unless entering 13th season or greater
+	    if(int(tempRow0["position"]) == pos or seasonNum > 12):
+	    	# Is this the same player as the last row
+		    if(prevId != myId):
+		    	tempX = []
+		    	samePlayer = False
+		    else:
+		    	samePlayer = True
+
+
+		    # Include if this season is less than current season for player
+		    if(mySeason < seasonNum):
+		    	# If is a new player
+		    	if(samePlayer == False):
+		    		# Add 0s to first season if they missed it
+		    		if(mySeason != 0):
+		    			if(mySeason-1 == 0):
+			    			tempX.append([0]*len(predictors))
+			    			tempX.append(vectorOfStats(tempRow0, predictors))
+			    	elif(mySeason == 0):
+			    		tempX.append(vectorOfStats(tempRow0, predictors))
+			    	else:
+			    		tempX = []
+
+			    # If not a new player
+		    	else:
+		    		if(mySeason-1 != prevSeason):
+		    			# Add 0s to missing seasons
+		    			for x in range(mySeason-1-prevSeason):
+		    				tempX.append([0]*len(predictors))
+		    		tempX.append(vectorOfStats(tempRow0, predictors))
+
+
+		    # If we have reached last season needed
+		    elif(mySeason == seasonNum):
+		    	
+		    	tempX.append(vectorOfStats(tempRow0, predictors))
+		    	tempX = np.array(tempX)
+		    	tempX = tempX.flatten()
+		    	# If we have a vector of proper size
+		    	if(len(tempX) == seasonNum*len(predictors)):
+		    		tempX = tempX.tolist()
+		    		if(seasonNum > 16):
+		    			tempX = tempX[(len(tempX)-numInputs):]
+		    		elif(seasonNum > 9):
+		    			tempX = tempX[(len(tempX)-numInputs):]
+
+		    		# Appending to the trainingX and trainingY
+			    	trainingX.append(tempX)
+			    	trainingY.append([int(tempRow0[stat1])])
+		    	tempX = []
+		    # Otherwise clear our vector
+		    else:
+		    	tempX = []
+
+
+	    prevId = myId
+	    prevSeason = mySeason
+	#---------End of getting stats from all players-----------#
+
+
+
+
+	#Training Data:
+	trainingX = np.asarray(trainingX)
+	trainingY = np.asarray(trainingY)
+
+	#Normalize:
+	xMax = np.amax(trainingX, axis=0)
+	yMax = np.amax(trainingY, axis=0)
+	copyTest = np.array(copyTest)
+	playerMax = np.amax(copyTest, axis=0)
+
+	# statAdjustDict1 = {'G':0, 'MP':4, 'PTS':5, 'ORB':.8, 'DRB':1.2, 'AST':1.6, 'STL':.3, 'BLK':.5, 'TOV':.25,'3P':.8}
+	# statAdjustDict2 = {'G':0, 'MP':2, 'PTS':2.5, 'ORB':.4, 'DRB':.8, 'AST':.8, 'STL':.15, 'BLK':.25, 'TOV':.15,'3P':.4}
+
+	def improvementFunction(num):
+		return (seasonNum-7)*(-num/5)
+
+	statAdjustDict = {
+						'G':0,
+						'MP':improvementFunction(300), 
+						'PTS':improvementFunction(400), 
+						'ORB':improvementFunction(35),
+						'DRB':improvementFunction(80), 
+						'AST':improvementFunction(65), 
+						'STL':improvementFunction(30), 
+						'BLK':improvementFunction(30), 
+						'TOV':improvementFunction(20),
+						'3P':improvementFunction(40)}
+
+	if(seasonNum < 8):
+		playerMax[0] += statAdjustDict[stat1]
+
+
+	trainingX = trainingX/xMax
+	trainingY = trainingY/yMax
+
+	NN = Neural_Network(Lambda = 0.001, inputSize = numInputs, hiddenSize = int((numInputs)/1.5), outputSize = 1)
+	T = trainer(NN)
+	T.train(trainingX, trainingY, trainingX, trainingY)
+
+	result = NN.propogation(testVector)
+	adjustedResult = (result*playerMax)
+	returnStat = round(adjustedResult[0], 1)
+
+
+	return returnStat
+
+	
+
+directory = 'player_stats/'
+csvFile = 'all-seasons.csv'
+allStats = pd.read_csv(csvFile)
+
+for f in sorted(os.listdir(directory)):
+
+	playerCSV = directory+f
+	playerStats = pd.read_csv(playerCSV)
+	firstName = f[:f.index('_')]
+	lastName = f[f.index('_')+1:f.index('.')]
+	playerName = (firstName + ' ' + lastName)
+	print(playerName + ' starting.')
+
+
+	stats = ['G', 'MP', 'PTS', '3P', 'ORB', 'DRB', 'AST', 'TOV', 'STL', 'BLK']
+	predictors = [['G', 'MP', 'PTS'],
+				['MP', 'G', 'PTS'],
+				['PTS', 'G', '3P'],
+				[ '3P', 'G', 'PTS'],
+				['ORB', 'G', 'DRB', 'BLK'],
+				['DRB', 'G', 'ORB', 'BLK'],
+				['AST', 'G', 'TOV', 'STL'],
+				['TOV', 'G', 'AST', 'STL'],
+				['STL', 'G', 'AST', 'TOV'],
+				['BLK', 'G', 'ORB', 'DRB'],
+				]
+	finalStats = []
+
+	for s in range(len(stats)):
+		statPrediction = predictStat(playerStats, allStats, stats[s], predictors[s])
+		finalStats.append(statPrediction)	
+		print(stats[s] + ': ' + str(statPrediction))
+
+
+	gamesPlayed = finalStats[0]
+	index = [playerName]
+	d = {'G' : pd.Series([round(gamesPlayed, 0)], index=index),
+		'MP' : pd.Series([round(finalStats[1]/gamesPlayed,1)], index=index),
+		'PTS' : pd.Series([round(finalStats[2]/gamesPlayed,1)], index=index),
+		'3P' : pd.Series([round(finalStats[3]/gamesPlayed,1)], index=index),
+		'ORB' : pd.Series([round(finalStats[4]/gamesPlayed,1)], index=index),
+		'DRB' : pd.Series([round(finalStats[5]/gamesPlayed,1)], index=index),
+		'AST' : pd.Series([round(finalStats[6]/gamesPlayed,1)], index=index),
+		'TOV' : pd.Series([round(finalStats[7]/gamesPlayed,1)], index=index),
+		'STL' : pd.Series([round(finalStats[8]/gamesPlayed,1)], index=index),
+		'BLK' : pd.Series([round(finalStats[9]/gamesPlayed,1)], index=index)
+		}
+
+	df = pd.DataFrame(d, columns=['G', 'MP', 'PTS', '3P', 'ORB', 'DRB', 'AST', 'TOV', 'STL', 'BLK'])
+	
+	with open('output2.csv', 'a') as out:
+		df.to_csv(out, header=False)
+	print(playerName + ' stats predicted.')
